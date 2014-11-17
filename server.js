5098aa44c28346fbbb1e64cb347947808ce795d6
prufrock123 <shawnmllr86@gmail.com>
2014-11-17 09:16:39 -0600

    qoipi

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ function startServer() {
                     })
                     console.log(match);
 
-                    // match.child('deliveryChoice').set('Home Delivery');
+                    match.child('deliveryChoice').set('Home Delivery');
 
                 // jquery.when(
                     //     packagesRef.orderByChild('phoneNumber').equalTo(fromNum).on('child_added', function(snapshot){
